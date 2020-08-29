c0081545e8ae57988572a7a0043943a4954f4762
Rocky-Biloba <ricafur@gmail.com>
2020-08-29 10:27:50 -0400

    V4
    probleme donner congé (fenetre medecin)

--- FenetreMedecin.xaml.cs
+++ FenetreMedecin.xaml.cs
@@ -23,11 +23,44 @@ namespace NorthernLightsHospital
         public FenetreMedecin()
         {
             InitializeComponent();
-            cb_IDmedecin.DataContext = Login.myBDD.tblMedecins.ToList();
+            //cb_IDmedecin.DataContext = Login.myBDD.tblMedecins.ToList();
             cb_choixLit.DataContext = Login.myBDD.tblLits.ToList();
             cb_choixLit.DataContext = Login.myBDD.tblLitTypes.ToList();
             cb_choixLit.DataContext = Login.myBDD.tblDepts.ToList();
             dp_dateAdmis.SelectedDate = DateTime.Today;
+
+            // all fields read-only, except NAS (recherche) + date congé 
+            gbox_coordPatient.IsEnabled = false;
+            gbox_admissionPatient.IsEnabled = false;
+            btn_conge.IsEnabled = false;
+            dp_conge.IsEnabled = false;
+
+            //// récrupère description du Lit
+            //var query = from temp in Login.myBDD.tblLits where temp.Occupe == false select temp;
+            //List<NorthernLightsHospital.tblLit> list_query = query.ToList();
+
+            //foreach (var i in list_query)
+            //{
+            //    string NewLine = i.Lit.ToString();
+
+            //    //Type
+            //    if (i.Type == 1)
+            //        NewLine += " | Standard      ";
+            //    else if (i.Type == 2)
+            //        NewLine += " | Semi privé";
+            //    else
+            //        NewLine += " | Privé         ";
+
+            //    //Department
+            //    if (i.IDdept == 1)
+            //        NewLine += " | Urgence";
+            //    else if (i.IDdept == 2)
+            //        NewLine += " | Réadaptation";
+            //    else
+            //        NewLine += " | Chirugier";
+
+            //    cb_choixLit.Items.Add(NewLine);
+            //}
         }
 
         // RECHERHER PATIENT (NAS)
@@ -35,12 +68,13 @@ namespace NorthernLightsHospital
         {
             if (!String.IsNullOrEmpty(tbox_NAS.Text))
             {
-                btn_ajouter.IsEnabled = false;
-                btn_admettre.IsEnabled = true;
+                // add the condition if dp_conge is !null here?
+                btn_conge.IsEnabled = true;
+               
 
                 int nasPatient = int.Parse(tbox_NAS.Text.Trim());
                 tblPatient pat = Login.myBDD.tblPatients.SingleOrDefault(x => x.NAS == nasPatient);
-                if (pat != null)
+                if (pat != null) // vérifier que le champ NAS n'est pas vide
                 {
                     string IDass = pat.IDassurance.ToString();
                     string tel = pat.Tel.ToString();
@@ -53,15 +87,48 @@ namespace NorthernLightsHospital
                     tbox_ville.Text = pat.Ville;
                     tbox_prov.Text = pat.Province;
                     tbox_CP.Text = pat.CP;
-                    tbox_tel.Text = tel;
+                    tbox_tel.Text = tel; 
+                    
+                    // AFFICHAGE des informations associé tblAdmission
+                    tblAdmission adm = Login.myBDD.tblAdmissions.SingleOrDefault(x => x.NAS == nasPatient);
+                    if (adm != null) // Si le patient est admis
+                    {
+                        string erreur = "";
+                        tb_Erreur.Text = erreur;
+                        dp_conge.IsEnabled = true;
+                        btn_conge.IsEnabled = true;
+                        dp_dateAdmis.SelectedDate = adm.dateAdmis;
+                        // vider combobox, affiche le IDmedecin associé à l'admission du patient
+                        cb_IDmedecin.ClearValue(ItemsControl.ItemsSourceProperty);
+                        cb_IDmedecin.Items.Add(adm);
+                        cb_IDmedecin.SelectedIndex = 0;
 
-                    afficheDetailsAdmission();
+                        if (adm.Chirugie == true)
+                        {
+                            chbox_chirugie.IsChecked = adm.Chirugie;
+                        }
+
+                        dp_chirugie.SelectedDate = adm.dateChirugie;
+
+                        // vider combobox, affiche les infos Lit (Lit, Descript, NomDept)
+                        cb_choixLit.ClearValue(ItemsControl.ItemsSourceProperty);
+                        cb_choixLit.Items.Add(adm);
+                        cb_choixLit.SelectedIndex = 0;
+
+                    }
+                    else {
+                        btn_conge.IsEnabled = false;
+                        dp_conge.IsEnabled = false;
+                        string erreur = " Ce patient n'est pas admis.";
+                        tb_Erreur.Text = erreur;
+                    }
+                   
                 }
                 else
                 {
-                    MessageBox.Show("Ce NAS de patient n'existe pas encore.\n Veuillez entrer leur info",
+                    MessageBox.Show("Ce NAS de patient n'existe pas.",
                     "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
-                    btn_ajouter.IsEnabled = true;
+                    btn_conge.IsEnabled = false;
                 }
 
             }
@@ -71,6 +138,46 @@ namespace NorthernLightsHospital
                 "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
             }
 
+        }
+
+        // DONNER CONGÉE
+        private void btn_conge_Click(object sender, RoutedEventArgs e)
+        {
+            int nasPatient = int.Parse(tbox_NAS.Text.Trim());
+            tblAdmission adm = Login.myBDD.tblAdmissions.SingleOrDefault(x => x.NAS == nasPatient);
+
+            // vérifie si l'usager as mis un date
+            if ((DateTime)dp_conge.SelectedDate == null)
+            {
+                // set to default date (today)
+                adm.dateConge = DateTime.Today;
+
+                MessageBox.Show("Patient" + nasPatient + "congédié aujourd'hui",
+                "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                adm.dateConge = (DateTime)dp_conge.SelectedDate;
+
+                MessageBox.Show("Patient" + nasPatient + "congédié (date)",
+                "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            //int nasPatient = int.Parse(tbox_NAS.Text.Trim());
+
+            //tblAdmission adm = Login.myBDD.tblAdmissions.SingleOrDefault(x => x.NAS == nasPatient);
+            //if (adm != null) // si le patient est admis
+            //{
+            //    dp_conge.SelectedDate = adm.dateConge;
+
+            //    MessageBox.Show("Patient" + nasPatient + "congédié",
+            //    "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
+            //else {
+
+            //    MessageBox.Show("erreur conner congé ?!",
+            //    "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
 
         }
 
@@ -81,11 +188,36 @@ namespace NorthernLightsHospital
             tb_NAS2.Text = NAS;
         }
 
-        // AFFICHER dans le section 'détails admission'
-        private void afficheDetailsAdmission() 
-        { 
-       // var query 
+        private void btn_annuler_Click(object sender, RoutedEventArgs e)
+        {
+            // vider les TextBox
+            tbox_NAS.Text = String.Empty;
+            tbox_prenom.Text = String.Empty;
+            tbox_nom.Text = String.Empty;
+            cb_IDassurance.Text = String.Empty;
+            tbox_adresse.Text = String.Empty;
+            tbox_ville.Text = String.Empty;
+            tbox_prov.Text = String.Empty;
+            tbox_CP.Text = String.Empty;
+            tbox_tel.Text = String.Empty;
 
+            // vider les combobox
+            cb_IDmedecin.Text = String.Empty;
+            cb_choixLit.Text = String.Empty;
+
+            //// décocher les checkbox
+            chbox_chirugie.IsChecked = false;
+            
+            // vider les datePicker
+            dp_DOB.SelectedDate = null;
+            dp_chirugie.SelectedDate = null;
+            dp_conge.SelectedDate = null;
+
+            // vider msg d'erreur
+            string erreur = "";
+            tb_Erreur.Text = erreur;
         }
+
+       
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,20 +20,48 @@ namespace NorthernLightsHospital
     /// </summary>
     public partial class MainWindow : Window
     {
-        string NAS; /*specMed;*/
-        //int idMed, typeLit;
+        string NAS;
         public MainWindow()
         {
             InitializeComponent();
             cb_IDmedecin.DataContext = Login.myBDD.tblMedecins.ToList();
-            cb_choixLit.DataContext = Login.myBDD.tblLits.ToList();
-            cb_choixLit.DataContext = Login.myBDD.tblLitTypes.ToList();
-            cb_choixLit.DataContext = Login.myBDD.tblDepts.ToList();
+            //cb_choixLit.DataContext = Login.myBDD.tblLits.ToList();  //{Lee}
+            //cb_choixLit.DataContext = Login.myBDD.tblLitTypes.ToList(); //
+            //cb_choixLit.DataContext = Login.myBDD.tblDepts.ToList();//
             dp_dateAdmis.SelectedDate = DateTime.Today;
+
+            // récrupère les LITS disponibles (Occupé = 0 ou false) 
+            var query = from temp in Login.myBDD.tblLits where temp.Occupe == false select temp;
+            List<NorthernLightsHospital.tblLit> list_query = query.ToList();
+
+            foreach (var i in list_query)
+            {
+                string NewLine = i.Lit.ToString();
+
+                //Type
+                if (i.Type == 1)
+                    NewLine += " | Standard      ";
+                else if (i.Type == 2)
+                    NewLine += " | Semi privé";
+                else 
+                    NewLine += " | Privé         ";
+
+                //Department
+                if (i.IDdept == 1)
+                    NewLine += " | Urgence";
+                else if (i.IDdept == 2)
+                    NewLine += " | Réadaptation";
+                else
+                    NewLine += " | Chirugier";
+
+                cb_choixLit.Items.Add(NewLine);
+            }
+            //int Hi = 3;
         }
 
-        // RECHERHER PATIENT (NAS)
-        private void btn_rechercher_Click(object sender, RoutedEventArgs e)
+
+            // RECHERHER PATIENT (NAS)
+            private void btn_rechercher_Click(object sender, RoutedEventArgs e)
         {
             if (!String.IsNullOrEmpty(tbox_NAS.Text))
             {
@@ -41,6 +69,7 @@ namespace NorthernLightsHospital
                 btn_admettre.IsEnabled = true;
 
                 int nasPatient = int.Parse(tbox_NAS.Text.Trim());
+
                 tblPatient pat = Login.myBDD.tblPatients.SingleOrDefault(x => x.NAS == nasPatient);
                 if (pat != null)
                 {
@@ -64,14 +93,15 @@ namespace NorthernLightsHospital
                     btn_ajouter.IsEnabled = true;
                 }
 
-            }
-            else
-            {
-                MessageBox.Show("Le NAS est requis pour faire la recherche!",
-                "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
+                }
+                else
+                {
+                    MessageBox.Show("Le NAS est requis pour faire la recherche!",
+                    "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
 
-        }
+            }
+        
 
         // AFFICHER NAS dans le section 'détails admission'
         private void tbox_NAS_TextChanged(object sender, TextChangedEventArgs e)
@@ -86,13 +116,13 @@ namespace NorthernLightsHospital
             // POP-UP WINDOW AJOUTER UNE RÉF PARENT
         }
 
-     
+        // AJOUTER PATIENT
         private void btn_ajouter_Click(object sender, RoutedEventArgs e)
         {
         //    btn_ajouter.IsEnabled = true;
         //    btn_Admis.IsEnabled = true;
 
-            // AJOUT PATIENT
+            
             tblPatient patient = new tblPatient();
 
             patient.NAS = int.Parse(tbox_NAS.Text);
@@ -108,58 +138,109 @@ namespace NorthernLightsHospital
 
             Login.myBDD.tblPatients.Add(patient);
 
-          
-
             try
             {
                 Login.myBDD.SaveChanges();
                 MessageBox.Show("Personne ajoutée avec succes!");
-
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show(ex.Message);
             }
 
         }
 
+
+        // ADMISSION PATIENT
         private void btn_Admis_Click(object sender, RoutedEventArgs e)
         {
-            //// AJOUT ADMISSION
-            tblAdmission admis = new tblAdmission();
-           
-            //tb_IDadmission.Text = autogénéré;
-            admis.NAS = int.Parse(tbox_NAS.Text);
-            admis.dateAdmis = (DateTime)dp_dateAdmis.SelectedDate;
-            admis.IDmedecin = int.Parse(cb_IDmedecin.Text);
-            admis.Lit = int.Parse(cb_choixLit.Text);
-            admis.Chirugie = chbox_chirugie.IsChecked;
-            admis.dateChirugie = (DateTime)dp_chirugie.SelectedDate;
-            admis.tv = Convert.ToBoolean(chbox_tv.IsChecked);
-            admis.telephone = Convert.ToBoolean(chbox_telChambre.IsChecked);
-            //admis.dateConge = (DateTime)dp_conge.Select
+            int nasPatient = int.Parse(tbox_NAS.Text.Trim());
 
-           Login.myBDD.tblAdmissions.Add(admis);
-
-
-            try
+            //Validation : que le patient n'est pas déjà admis
+            tblAdmission adm = Login.myBDD.tblAdmissions.SingleOrDefault(x => x.NAS == nasPatient);
+            if (adm != null) //// patient déjà admis
             {
-                Login.myBDD.SaveChanges();
-                MessageBox.Show("Personne admise avec succes!");
+                btn_admettre.IsEnabled = false; // Validation : usager ne peux pas admettre un patient déjà admis
+                MessageBox.Show("Ce patient est déjà admis.",
+            "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+                // vider le textbox NAS et reactiver le bouton ajouter
+                tbox_NAS.Text = String.Empty;
 
             }
-            catch (Exception ex)
-            {
+            else {
 
-                MessageBox.Show(ex.Message);
-            }
+                string LitNum = cb_choixLit.Text;
+                int index = LitNum.IndexOf(" |");
+                if (index > 0)
+                {
+                    LitNum = LitNum.Substring(0, index);
+                }
+
+                tblAdmission admis = new tblAdmission();
+
+                
+
+                //tb_IDadmission.Text = autogénéré
+                admis.NAS = int.Parse(tbox_NAS.Text);
+                admis.dateAdmis = (DateTime)dp_dateAdmis.SelectedDate;
+                admis.IDmedecin = int.Parse(cb_IDmedecin.Text);
+
+                admis.Lit = int.Parse(LitNum);
+                admis.Chirugie = chbox_chirugie.IsChecked;
+                //null date time fix + validation
+                if (chbox_chirugie.IsChecked == true)
+                {
+                    // vérifie si l'usager as mis un date
+                    if ((DateTime)dp_chirugie.SelectedDate == null)
+                    {
+                        // set to default date (today)
+                        admis.dateChirugie = DateTime.Today;
+                    }
+                    else
+                    {
+                        admis.dateChirugie = (DateTime)dp_chirugie.SelectedDate;
+                    }
+                }
+                // else set date to null
+                else
+                {
+                    admis.dateChirugie = null;
+                }
+                admis.tv = Convert.ToBoolean(chbox_tv.IsChecked);
+                admis.telephone = Convert.ToBoolean(chbox_telChambre.IsChecked);
+
+                Login.myBDD.tblAdmissions.Add(admis);
+
+                try
+                {
+                    Login.myBDD.SaveChanges();
+                    MessageBox.Show("Personne admise avec succes!");
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            } 
 
         }
 
         //ANNULER
         private void btn_annuler_Click(object sender, RoutedEventArgs e)
         {
+            ViderTout();
+            btn_admettre.IsEnabled = true;
+        }
+
+        private void cb_IDmedecin_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //idMed = int.Parse(cb_IDmedecin.Text);
+            //specMed = MenuPrepose.myBDD.tblMedecins.Specialite.ToString();
+            //tb_nomSpecMedecin.DataContext = specMed;
+        }
+
+        public void ViderTout() {
             // vider les TextBox
             tbox_NAS.Text = String.Empty;
             tbox_prenom.Text = String.Empty;
@@ -171,25 +252,18 @@ namespace NorthernLightsHospital
             tbox_CP.Text = String.Empty;
             tbox_tel.Text = String.Empty;
 
-            //// déselectionner les boutons radio
-            //rb_chirugie.IsChecked = false;
-            //rb_standard.IsChecked = false;
-            //rb_semiprive.IsChecked = false;
-            //rb_prive.IsChecked = false;
-            //rb_tv.IsChecked = false;
-            //rb_telChambre.IsChecked = false;
-            
-            //// vider les datePicker
-            //dp_DOB.SelectedDate = null;
-            //dp_chirugie.SelectedDate = null;
-            //dp_conge.SelectedDate = null;
-        }
+            // vider les combobox
+            cb_IDmedecin.Text = String.Empty;
+            cb_choixLit.Text = String.Empty;
 
-        private void cb_IDmedecin_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //idMed = int.Parse(cb_IDmedecin.Text);
-            //specMed = MenuPrepose.myBDD.tblMedecins.Specialite.ToString();
-            //tb_nomSpecMedecin.DataContext = specMed;
+            //// décocher les checkbox
+            chbox_chirugie.IsChecked = false;
+            chbox_tv.IsChecked = false;
+            chbox_telChambre.IsChecked = false;
+
+            // vider les datePicker
+            dp_DOB.SelectedDate = null;
+            dp_chirugie.SelectedDate = null;
         }
 
        
