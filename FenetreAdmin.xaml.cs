1ee6d2873a47478c440a23b1fa464bc8852fbce5
Rocky-Biloba <ricafur@gmail.com>
2020-08-30 15:57:40 -0400

    V7
    fonctionelle
    ajustement à faire..

--- FenetreAdmin.xaml.cs
+++ FenetreAdmin.xaml.cs
@@ -25,14 +25,11 @@ namespace NorthernLightsHospital
             InitializeComponent();
             cb_IDmedecin.DataContext = Login.myBDD.tblMedecins.ToList();
 
-            // liste de spécilaités
-            //List<string> listSpecialites = new List<string>();
-            //listSpecialites.Add("");
-            //cb_specialite.DataSource = listSpecialites;
+            // liste de spécialités
             cb_specialite.Items.Add("Interniste");
             cb_specialite.Items.Add("Pédiatrie");
             cb_specialite.Items.Add("Anesthésiologie");
-            
+
         }
 
         private void cb_IDmedecin_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -41,6 +38,7 @@ namespace NorthernLightsHospital
 
         }
 
+        // SUPPRIMER
         private void btn_supprimer_Click(object sender, RoutedEventArgs e)
         {
             // récupère le int IDmedecin du combobox
@@ -52,12 +50,15 @@ namespace NorthernLightsHospital
                 where admis.IDmedecin == IDmed
                 select admis;
 
-            if (query != null)
+            List<NorthernLightsHospital.tblAdmission> list_query = query.ToList();
+
+            if (list_query.Count > 0)
             {
                 string erreur = "Docteur " + IDmed + " ne peux pas\nêtre congédié en raison d'avoir les patients\nactuellement admis.";
-                tb_erreur.Text = erreur;  
+                tb_erreur.Text = erreur;
             }
-            else {
+            else
+            {
                 string erreur = "";
                 tb_erreur.Text = erreur;
                 // Query pour SUPPRIMER un médecin
@@ -74,8 +75,8 @@ namespace NorthernLightsHospital
                 try
                 {
                     Login.myBDD.SaveChanges();
-
-                    MessageBox.Show("Docteur " + IDmed + " congédié le : " + DateTime.Today.ToString("YYYY MM DD"),
+                    cb_IDmedecin.DataContext = Login.myBDD.tblMedecins.ToList();
+                    MessageBox.Show("Docteur " + IDmed + " congédié le : " + DateTime.Today.ToString("yyyy MM dd"),
                        "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
                 catch (Exception f)
@@ -86,6 +87,28 @@ namespace NorthernLightsHospital
             }
         }
 
-      
+        private void btn_ajouter_Click(object sender, RoutedEventArgs e)
+        {
+            tblMedecin medecin = new tblMedecin();
+
+            medecin.Nom = tbox_nom.Text;
+            medecin.Prenom = tbox_prenom.Text;
+            medecin.Specialite = cb_specialite.Text;
+
+            Login.myBDD.tblMedecins.Add(medecin);
+
+            try
+            {
+                Login.myBDD.SaveChanges();
+                cb_IDmedecin.DataContext = Login.myBDD.tblMedecins.ToList();
+                MessageBox.Show("Médecin ajouté avec succes!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
+
